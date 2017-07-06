a9e92ff8600d9a722fa566961c8f2a7d09f21629
wuxuejian <wuxuejian@yy.com>
2017-07-06 12:06:44 +0800

    修改gradle中lint的设置为false

--- app/src/main/java/com/wxj/hello/MainActivity.java
+++ app/src/main/java/com/wxj/hello/MainActivity.java
@@ -3,11 +3,16 @@ package com.wxj.hello;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List array = new ArrayList<>();
     }
 }
